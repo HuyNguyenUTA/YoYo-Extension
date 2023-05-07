fcf327bd41ea5e24eaa4cdc9b5e4d6b458523597
HuyNguyenUTA <huy.nguyen.uta@gmail.com>
2023-05-07 00:34:15 -0500

    bug-no-end-connection-to-render-extension

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -1,51 +1,31 @@
-import styles from "../../next-app/styles/Home.module.css"
-import React from 'react';
+import styles from "../../next-app/styles/Home.module.css";
+import React, { useState, useEffect } from "react";
 
-const IndexPage: React.FC = () => {
+interface Message {
+  action: string;
+  inputValue: string;
+}
+
+export default function Home() {
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    console.log("useEffect triggered");
+    chrome.runtime.onMessage.addListener(
+      (message: Message, sender, sendResponse) => {
+        console.log(message.inputValue)
+        if (message.action === "setInputValue") {
+          setInputValue(message.inputValue);
+        }
+      }
+    );
+    chrome.runtime.sendMessage({ action: "setQueryParamValue" });
+  }, []);
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+    <div>
+      <h1>{inputValue}</h1>
+      <h1>Hello Huy</h1>
     </div>
   );
-};
-
-export default IndexPage;
+}
